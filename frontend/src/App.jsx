d5fb072f175b339afe85a316c66b4da0466dfc6a
Duc <e1846079@cmaisonneuve.qc.ca>
2024-03-27 13:01:37 -0400

    added features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import React, {useEffect} from 'react'
-import Login from './login'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import necessary components
+import Home from "./Home";
+import Page2 from "./Page2";
+import Login from "./Login";
 
-function App(){
-  useEffect(()=> {
-    fetch('http://localhost:3000/Clients')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-  })
-  return (
-    <div>
-      <Login />
-    </div>
-  )
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter> {/* Use BrowserRouter */}
+                <Routes> {/* Use Routes */}
+                    <Route path="/" element={<Login />} />
+                    <Route path="/Home" element={<Home />} /> 
+                    <Route path="/Page2" element={<Page2 />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
