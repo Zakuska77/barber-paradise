5b0f5cac0688d81657997dd5265a6c70a77d51c5
Duc-Anh Vu <e1846079@cmaisonneuve.qc.ca>
2024-04-03 21:48:36 +0000

    Update Home.jsx

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
-=======
-import React, { useState, useEffect, useContext } from "react";
->>>>>>> 88659767bd5984875350f2e0eeb3cf021035a30f
-=======
-import React, { useState, useEffect, useContext } from "react";
->>>>>>> 88659767bd5984875350f2e0eeb3cf021035a30f
 import { api } from "../api/api";
 import CoiffeurCarte from "../components/CoiffeurCarte";
 import { useNavigate } from "react-router-dom";
@@ -43,7 +35,6 @@ function App() {
       console.log('error', inputData)
     }
   }
-  console.log(localStorage.getItem('token'));
 
   return (
     <>
